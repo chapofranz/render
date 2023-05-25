979e4545eb7f99e3264e4068758bd9584d32f406
Charalampos Papadakis <ch121pap@htwg-konstanz.de>
2023-05-25 13:20:52 +0200

    JustGoOnlineThenFinished

--- api/controllers/check/user-name-exists.js
+++ api/controllers/check/user-name-exists.js
@@ -1,35 +1,41 @@
 module.exports = {
-    friendlyName: 'Check Username Exists',
-    description: 'Check if a username is already in use.',
-    inputs: {
-      userName: {
-        required: true,
-        type: 'string',
-        description: 'The username to check.',
-      },
+  friendlyName: 'Check Username Exists',
+  description: 'Check if a username is already in use.',
+  inputs: {
+    userName: {
+      required: true,
+      type: 'string',
+      description: 'The username to check.',
     },
-    exits: {
-      userNameExists: {
-        responseType: 'conflict',
-        description: 'The provided username is already in use.',
-      },
-      userNameDoesNotExist: {
-        responseType: 'notFound',
-        description: 'The provided username does not exist.',
-      },
+  },
+  exits: {
+    userNameExists: {
+      responseType: 'conflict',
+      description: 'The provided username is already in use.',
     },
-    fn: async function ({ userName }) {
+    userNameDoesNotExist: {
+      responseType: 'notFound',
+      description: 'The provided username does not exist.',
+    },
+  },
+  fn: async function ({ userName }) {
+
     
-      var user = await User.findOne({
-        userName: userName
-      });
-      
-      
-  
-      if(user) {
-        throw 'userNameExists';
+    
+    var user = await User.findOne({
+      userName: userName
+    });
+
+
+
+    if(user) {
+      if(this.req.me && user.id === this.req.me.id) {
+        throw 'userNameDoesNotExist'; 
       } else {
-        throw 'userNameDoesNotExist';
+        throw 'userNameExists'; 
       }
+    } else {
+      throw 'userNameDoesNotExist'; 
     }
-  };
+  }
+};
