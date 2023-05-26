e2daad659415e029ebd3d04125e199e5ad9387cd
CR00K3D <nicolas.quilitzsch@gmx.de>
2023-05-26 11:36:56 +0200

    y

--- api/controllers/DozentenController.js
+++ api/controllers/DozentenController.js
@@ -3,7 +3,7 @@ const Sails = require("sails/lib/app/Sails");
 module.exports = {
   list: async function (req, res) {
     try {
-      const dozenten = await Dozent.find();
+      const dozenten = await Dozent.find().populate("studiengang");
       res.view('pages/dozenten/show', { dozenten });
     } catch (error) {
       res.serverError(error);
